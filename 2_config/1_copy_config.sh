f1816471f9cc6903c814bcb791bb1ba0e533c18d
Fernando Karnagi <fkarnagi@gmail.com>
2025-02-13 11:52:52 +0800

    Update config

--- 2_config/1_copy_config.sh
+++ 2_config/1_copy_config.sh
@@ -1,1 +1,8 @@
 cp 2_config/*conf /etc/
+mkdir -p /var/log/mongodb/cfg/logs
+mkdir -p /var/lib/mongodb/cfg/db
+mkdir -p /var/lib/mongodb/A/db
+mkdir -p /var/log/mongodb/A/logs
+mkdir -p /var/lib/mongodb/B/db
+mkdir -p /var/log/mongodb/B/logs
+mkdir -p /var/log/mongodb/mongos/logs
